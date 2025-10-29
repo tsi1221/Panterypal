95745112cc52f268977aa9ac0e17c63fda41b087
Tsehaynesh Biruh <tsehayneshbiruh2@gmail.com>
2025-10-29 08:20:00 -0700

    Add error handling middleware

---
+++ src/middleware/errorMiddleware.js
@@ -0,0 +1,8 @@
+// /src/middleware/errorMiddleware.js
+export const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Server Error",
+  });
+};
